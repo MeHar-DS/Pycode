450a791f176972ed0f20ae5248166169e66c2ab3
mervinh <mervaqua1987@gmail.com>
2023-02-17 15:43:28 +0530

    Adding changes to PyPractice

--- PyPractice.py
+++ PyPractice.py
@@ -1,9 +1,10 @@
-#variables
-#Comments
+# variables
+# Comments
 print("this is a Hello from Python World!")
 print("##This is not a comment since it is inside quotes!")
-#integer type
-i=3
-print("Value of integer i:",i,type(i))
-f=.5
-print("Value of float f:",f,type(f))
+# integer type
+i = 3
+print("Value of integer i:", i, type(i))
+f = .5
+print("Value of float f:", f, type(f))
+
