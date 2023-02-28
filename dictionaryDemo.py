0600f2018cceee51c48e6e1783ceadc698150d04
mervinh <mervaqua1987@gmail.com>
2023-02-28 21:25:55 +0530

    Added new files dictionaryDemo and SetDemo

---
+++ dictionaryDemo.py
@@ -0,0 +1,63 @@
+# Dictionaries are indexed by keys, which can be any immutable type;
+# strings and numbers can always be keys.
+# Tuples can be used as keys if they contain only strings, numbers, or tuples;
+# if a tuple contains any mutable object either directly or indirectly, it cannot be used as a key.
+# You can’t use lists as keys, since lists can be modified in place using
+# index assignments, slice assignments, or methods like append() and extend()
+
+# It is best to think of dictionaries as Key value pairs with the requirement that
+# keys are unique within one dictionary
+# A pair of braces creates an empty dictionary {}
+
+# The main operations for a dictionary are storing a value with some key and extracting the value given the key
+# It is also possible to delete a key value pair with the del
+# If you store a value with an already existing key then the old value is replaced
+# It is an error to extract a non-existing key
+
+# Performing list(d) on a dictionary creates a list with the keys used in the dictionary, in insertion order
+# If you want the list to be sorted then sorted(d) can be used
+# To check whether a key is existing in dictionary use the in keyword
+
+
+tel = {'jack': 4098, 'sape': 4139}
+
+tel['guido'] = 4127
+print(tel)
+
+print(tel['jack'])
+
+del tel['sape']
+
+print(tel)
+
+tel['irv'] =4127
+
+print(tel)
+
+print(list(tel))  # This prints the list of keys in the inserted order
+
+print(sorted(tel))
+
+print("guido" in tel)
+
+print("jack" not in tel)
+
+
+# The dict() constructor builds dictionaries directly from sequences of key-value pairs
+
+dictfromseq = dict([('sape', 4139),('guido',4127), ('jack', 4098)])
+
+print(dictfromseq)
+
+# Dictionary comprehensions can be used to create dictionaries from arbitrary key and value expressions
+
+dictcomp = {x: x**2 for x in range(1, 10)}
+
+print(dictcomp)
+
+
+# When the keys are simple strings it is sometimes easier to specify pairs using keyword arguments
+
+dictstrings = dict(John=1111, Don=2222, Sam=3333)
+
+print(dictstrings)
--- setDemo.py
+++ setDemo.py
@@ -6,5 +6,31 @@
 
 basket = {"apple", "orange", "apple", "pear", "orange", "banana"}
 
+print(basket)  # To demonstrate that duplicates are removed
+
+print("orange" in basket)
+
+print("crabgrass" in basket)
+
+# Demonstrate set operations on unique letters from two words
+
+a = set("abracadabra")
+b = set("alacazam")
+
+print(a)  # This prints unique letters in set a
+print(b)  # This prints unique letters in set b
+
+print(a-b)  # print unique letters in a but not in b
+
+print(a | b)  # print unique letters in a or b or both
+
+print(a & b)  # print unique letters in both a and b
+
+print(a ^ b)  # print unique letters in a or b but not in both
 
 
+# Similarly to list comprehensions there is set comprehensions
+
+setcomp = {x for x in "abracadabra" if x not in "abc"}
+
+print(setcomp)
